762e2e2a16162003393bbb37b4724491348f0d88
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-12-03 15:34:43 +0000

    Add advanced error handling with Apollo Link

--- app/_lib/graphql/apolloClient.ts
+++ app/_lib/graphql/apolloClient.ts
@@ -1,8 +1,26 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache, from } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    );
+  if (networkError) console.error(`[Network error]: ${networkError}`);
+});
+
+const httpLink = new HttpLink({ uri: "https://graphql.anilist.co" });
 
 const client = new ApolloClient({
-  uri: "https://graphql.anilist.co",
   cache: new InMemoryCache(),
+  link: from([errorLink, httpLink]),
 });
 
 export default client;
+
+// const client = new ApolloClient({
+//   uri: "https://graphql.anilist.co",
+//   cache: new InMemoryCache(),
+// });
